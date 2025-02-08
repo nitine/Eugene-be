9b4fe638046393e4855f926378e4d8eb3d02d22d
nitine <nitin.spaced@gmail.com>
2025-02-08 11:58:48 +0530

    fix deployment err

--- mygene/settings.py
+++ mygene/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
